0c4f7689fb4cb81b45fcd9f7855b2d1302f45a86
ilias <ilias@192.168.1.16>
2022-05-29 21:22:15 +0000

    implementation of the sprint2/US1

---
+++ tennis/src/main/java/com/kata/tennis/model/SetScoreHistory.java
@@ -0,0 +1,47 @@
+package com.kata.tennis.model;
+
+public class SetScoreHistory {
+	private Game game;
+	private Integer initialPlayer1SetScore;
+	private Integer initialPlayer2SetScore;
+	private Integer finalPlayer1SetScore;
+	private Integer finalPlayer2SetScore;
+	//private Integer player1TieBreakScore;
+	//private Integer player2TieBreakScore;
+	
+	public SetScoreHistory(Game game,Integer  initialPlayer1SetScore, Integer initialPlayer2SetScore){
+		this.game = game;
+		this.initialPlayer1SetScore = initialPlayer1SetScore;
+		this.initialPlayer2SetScore = initialPlayer2SetScore;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public Integer getInitialPlayer1SetScore() {
+		return initialPlayer1SetScore;
+	}
+
+	public Integer getInitialPlayer2SetScore() {
+		return initialPlayer2SetScore;
+	}
+
+	public Integer getFinalPlayer1SetScore() {
+		return finalPlayer1SetScore;
+	}
+
+	public Integer getFinalPlayer2SetScore() {
+		return finalPlayer2SetScore;
+	}
+	
+	public void setFinalScore(Integer finalPlayer1Score,Integer finalPlayer2Score) {
+		this.finalPlayer1SetScore = finalPlayer1Score;
+		this.finalPlayer2SetScore = finalPlayer2Score;
+	}
+
+	
+	
+	
+	
+}
